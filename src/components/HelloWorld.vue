b7c978b7fe2ed59ddb1ea8f1b18a569f0281f6bf
chetvergov <realberg@yandex.ru>
2022-12-12 20:37:53 +0300

    1232

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,19 +33,19 @@
               >Тикер</label
             >
             <div class="mt-1 relative rounded-md shadow-md">
-              <form @submit.prevent="addCryproTocurrencies">
-                <input
-                  v-model="searchbar"
-                  type="text"
-                  name="wallet"
-                  id="wallet"
-                  class="block w-full p-2 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                  placeholder="Например DOGE"
-                />
-              </form>
+              <input
+                v-model="searchbar"
+                @keydown.enter="addCryproTocurrencies"
+                type="text"
+                name="wallet"
+                id="wallet"
+                class="block w-full p-2 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                placeholder="Например DOGE"
+              />
             </div>
             <div
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+              v-if="cryptosBages.length"
+              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap absolute"
             >
               <span
                 v-for="bage in cryptosBages"
@@ -62,6 +62,7 @@
           </div>
         </div>
         <button
+          @click="addCryproTocurrencies"
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
@@ -80,23 +81,57 @@
           </svg>
           Добавить
         </button>
+        <p>
+          <label for="filter">
+            Фильтр
+            <input v-model="filter" id="filter" />
+          </label>
+        </p>
+        <p>
+          <button
+            @click="currentPage = currentPage - 1"
+            type="button"
+            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            :disabled="!(currentPage > 1)"
+          >
+            Назад
+          </button>
+          <button
+            @click="currentPage += 1"
+            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            type="button"
+            :disabled="!hasNextPage"
+          >
+            Вперед
+          </button>
+        </p>
       </section>
 
-      <hr class="w-full border-t border-gray-600 my-4" />
+      <hr
+        v-if="filteredCurrencies.length"
+        class="w-full border-t border-gray-600 my-4"
+      />
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         <div
-          v-for="item in currencies"
-          :key="item.cost"
+          v-for="item in filteredCurrencies()"
+          :key="item.name"
+          @click="selectTicker(item)"
+          :class="{
+            'border-4': selectedTicker === item,
+          }"
           class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
         >
           <div class="px-4 py-5 sm:p-6 text-center">
             <dt class="text-sm font-medium text-gray-500 truncate">
-              {{ item }} - USD
+              {{ item.name }} - USD
             </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">100</dd>
+            <dd class="mt-1 text-3xl font-semibold text-gray-900">
+              {{ item.cost }}
+            </dd>
           </div>
           <div class="w-full border-t border-gray-200"></div>
           <button
+            @click.stop="deleteTicker(item)"
             class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
           >
             <svg
@@ -116,17 +151,25 @@
         </div>
       </dl>
       <hr class="w-full border-t border-gray-600 my-4" />
-      <section class="relative">
+      <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          VUE - USD
+          {{ selectedTicker.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div class="bg-purple-800 border w-10 h-24"></div>
-          <div class="bg-purple-800 border w-10 h-32"></div>
-          <div class="bg-purple-800 border w-10 h-48"></div>
-          <div class="bg-purple-800 border w-10 h-16"></div>
+          <div
+            v-for="graph in graphs"
+            :key="graph * Date.now()"
+            class="bg-purple-800 border w-10"
+            :style="{
+              height: `${graph}px`,
+            }"
+          ></div>
         </div>
-        <button type="button" class="absolute top-0 right-0">
+        <button
+          @click="selectedTicker = null"
+          type="button"
+          class="absolute top-0 right-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -164,9 +207,15 @@ export default defineComponent({
   },
   data() {
     return {
-      currencies: ["BTC", "DOGE"],
+      currencies: [{ name: "BTC", cost: 100 }],
       fullListCrypto: [] as string[],
       searchbar: "",
+      selectedTicker: null as { name: string; cost: number } | null,
+      graphs: [] as number[],
+      timer: 0,
+      filter: "",
+      currentPage: 1,
+      hasNextPage: true,
     };
   },
   computed: {
@@ -183,11 +232,45 @@ export default defineComponent({
     },
     alreadyIncurrencies(): boolean {
       return this.currencies.some(
-        (item) => item.toLowerCase() === this.searchbar.toLowerCase()
+        (item) => item.name.toLowerCase() === this.searchbar.toLowerCase()
+      );
+    },
+  },
+  watch: {
+    selectedTicker() {
+      this.graphs.length = 0;
+      clearInterval(this.timer);
+      if (this.selectedTicker) this.showGraphs();
+    },
+    filter(neww, old) {
+      this.currentPage = 1;
+      window.history.pushState(
+        null,
+        "",
+        `${window.location.pathname}?filter=${this.filter}&currentPage=${this.currentPage}`
+      );
+    },
+    currentPage() {
+      window.history.pushState(
+        null,
+        "",
+        `${window.location.pathname}?filter=${this.filter}&currentPage=${this.currentPage}`
       );
     },
   },
   methods: {
+    filteredCurrencies() {
+      const start = (this.currentPage - 1) * 3;
+      const end = this.currentPage * 3;
+
+      const filteredCurrencies = this.currencies.filter((item) =>
+        item.name.toLowerCase().includes(this.filter.toLowerCase())
+      );
+
+      this.hasNextPage = filteredCurrencies.length > end;
+
+      return filteredCurrencies.slice(start, end);
+    },
     async getFullListCrypto() {
       const res = await fetch(
         "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
@@ -195,25 +278,69 @@ export default defineComponent({
       const body = await res.json();
       this.fullListCrypto = Object.keys(body.Data);
     },
+    deleteTicker(ticker: { name: string; cost: number }) {
+      this.currencies = this.currencies.filter((item) => item !== ticker);
+
+      if (ticker === this.selectedTicker) this.selectedTicker = null;
+
+      localStorage.setItem("currencies", JSON.stringify(this.currencies));
+    },
     addBage(bage: string) {
       this.searchbar = bage;
     },
     addCryproTocurrencies(value = "") {
-      const newItem = typeof value === "string" ? value : this.searchbar;
+      this.filter = "";
+      const newItem = {
+        name: typeof value === "string" ? value : this.searchbar,
+        cost: 10,
+      };
+
       if (
-        this.fullListCrypto.some(
-          (item) => item.toLowerCase() === this.searchbar.toLowerCase()
-        ) &&
+        newItem.name &&
         !this.currencies.some(
-          (item) => item.toLowerCase() === newItem.toLowerCase()
+          (item) => item.name.toLowerCase() === newItem.name.toLowerCase()
         )
       ) {
         this.currencies.push(newItem);
+        this.searchbar = "";
+        localStorage.setItem("currencies", JSON.stringify(this.currencies));
       }
+    },
+    showGraphs() {
+      this.timer = setInterval(() => {
+        const randomCost = Math.ceil(Math.random() * 255);
+        if (this.graphs.length >= 37) {
+          this.graphs.shift();
+        }
+        this.graphs.push(randomCost);
+      }, 1000);
+    },
+    selectTicker(item: { name: string; cost: number }) {
+      this.selectedTicker
+        ? (this.selectedTicker = null)
+        : (this.selectedTicker = item);
     },
   },
   mounted() {
     this.getFullListCrypto();
+    if (localStorage.getItem("currencies")) {
+      const localCurrencies = JSON.parse(
+        localStorage.getItem("currencies") || ""
+      );
+      this.currencies = localCurrencies;
+    }
+
+    const windowData = Object.fromEntries(
+      new URL(window.location.href).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.currentPage) {
+      this.currentPage = +windowData.currentPage;
+    }
   },
 });
 </script>
